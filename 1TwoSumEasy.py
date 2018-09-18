d2614948e6224497eed35e2d8c07ebd55a7eb50f
扈晓晖 <563255387@qq.com>
2018-09-18 17:21:33 +0800

    add some thinking

--- 1TwoSumEasy.py
+++ 1TwoSumEasy.py
@@ -1,6 +1,15 @@
 # -*- coding: utf-8 -*-
 
 
+"""
+思路
+方法1：暴力解法，两两遍历数组当中的数，相加为target时返回，时间复杂度O(n^2) 空间复杂度O(1)
+方法2：使用一个map（即dict）结构存储“数值：下标”数据对，这样只需遍历两遍数组，
+        查找target-数值在不在dict中，时间复杂度O(n) 空间复杂度O(n)
+方法3：只遍历一遍数组，便遍历，便将“数值：下标”数据对加入dict中，找到答案时立刻返回 时间复杂度O(n) 空间复杂O(n)
+"""
+
+
 def twoSum(nums, target):
     lens = len(nums)
     for i in range(0, lens):
@@ -13,7 +22,7 @@ def twoSum2(nums, target):
     lens = len(nums)
     d = {}
     for i in range(0, lens):
-        d[nums[i]] = i;
+        d[nums[i]] = i
     for i in range(0, lens):
         x = target - nums[i]
         if x in d:
