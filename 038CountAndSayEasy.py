c488b159bcc817a1d691886a9614793d4139c09c
扈晓晖 <563255387@qq.com>
2018-09-22 10:39:37 +0800

    add 38

---
+++ 038CountAndSayEasy.py
@@ -0,0 +1,42 @@
+# coding=utf-8
+
+
+"""
+思路
+使用strs字符串保存上一次的结果，然后对strs进行分析，依次遍历字符，看当前字符是否与下一个字符相等，若相等则nums记数加一，
+若不相等，则将当前记数的nums与当前字符str[i]合并输出到返回字符串ret，最后处理最后一个字符，将它和倒数第二个字符比较
+
+另：考虑递归实现
+"""
+
+
+def countAndSay(n):
+    x = 1
+    strs = "1"
+    ret = ""
+    if n == 1:
+        return strs
+    while x < n:
+        ret = ""
+        nums = 1
+        if len(strs) == 1:
+            ret = '1' + strs[0]
+        else:
+            for i in range(0, len(strs) - 1):
+                if strs[i] == strs[i + 1]:
+                    nums += 1
+                else:
+                    ret = ret + str(nums) + strs[i]
+                    nums = 1
+            if strs[-1] == strs[-2]:
+                ret = ret + str(nums) + strs[-1]
+            else:
+                ret = ret + '1' + strs[-1]
+        x += 1
+        strs = ret
+
+    return ret
+
+
+if __name__ == '__main__':
+    print(countAndSay(1))
