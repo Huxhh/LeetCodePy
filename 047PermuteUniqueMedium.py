25f4b32082d5de1ccdfbf22369d1f0ac4d9f0e45
扈晓晖 <563255387@qq.com>
2018-10-19 23:26:00 +0800

    add 046，47

---
+++ 047PermuteUniqueMedium.py
@@ -0,0 +1,26 @@
+# coding=utf-8
+
+
+def permuteUnique(nums):
+    ans = []
+
+    def backtrack(ans, temp, nums):
+        if len(temp) == len(nums):
+            ans.append(temp[:])
+        else:
+            for i in range(0, len(nums)):
+                if nums[i] in temp:
+                    continue
+                else:
+                    temp.append(nums[i])
+                    backtrack(ans, temp, nums)
+                    temp.pop()
+
+    backtrack(ans, [], nums)
+
+    return ans
+
+
+if __name__ == '__main__':
+    nums = [1, 2, 3]
+    print(permuteUnique(nums))
