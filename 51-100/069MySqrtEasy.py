d191ddafe0e4d52aa7cb7c82a0db9b6419bfc46b
扈晓晖 <563255387@qq.com>
2018-11-24 00:10:10 +0800

    add 069 thinking

--- 51-100/069MySqrtEasy.py
+++ 51-100/069MySqrtEasy.py
@@ -1,5 +1,11 @@
 # coding=utf-8
 
+"""
+思路：
+方法一：暴力，会超时
+方法二：二分查找
+方法三：牛顿法，详见https://en.wikipedia.org/wiki/Integer_square_root
+"""
 
 def mySqrt(x):
     i = 1
@@ -8,5 +14,32 @@ def mySqrt(x):
     return i - 1
 
 
+def mySqrt2(x):
+    if x < 2:
+        return x
+    l = 0
+    r = x // 2
+    ans = 1
+    while l <= r:
+        mid = (l + r) // 2
+        if mid * mid > x:
+            r = mid - 1
+        elif mid * mid < x:
+            l = mid + 1
+            ans = mid
+        else:
+            return mid
+    return ans
+
+
+def mySqrt3(x):
+    if x < 2:
+        return x
+    ans = x // 2
+    while ans > x / ans:
+        ans = (ans + x / ans) // 2
+    return int(ans)
+
+
 if __name__ == '__main__':
-    print(mySqrt(4))
+    print(mySqrt3(10))
